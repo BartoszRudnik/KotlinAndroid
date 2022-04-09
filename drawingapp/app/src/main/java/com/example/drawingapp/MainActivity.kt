b2efd339fa34f896b32ccd9d2cddd23a37b372af
Bartosz Rudnik <rudnik49@gmail.com>
2022-04-09 16:06:59 +0200

    /

--- drawingapp/app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ drawingapp/app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -4,17 +4,29 @@ import android.app.Dialog
 import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
+import androidx.core.view.get
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     private var drawingView: DrawingView? = null
+    private var mImageButtonCurrentPaint : ImageButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val linearLayoutPaintColors = findViewById<LinearLayout>(R.id.ll_paint_colors)
+
         drawingView = findViewById(R.id.drawingView)
+        mImageButtonCurrentPaint = linearLayoutPaintColors[1] as ImageButton
+        mImageButtonCurrentPaint!!.setImageDrawable(
+            ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
+        )
+
         drawingView?.setSizeForBrush(20.toFloat())
 
         val ibBrush : ImageButton = findViewById(R.id.ib_brush)
@@ -51,4 +63,23 @@ class MainActivity : AppCompatActivity() {
 
         brushDialog.show()
     }
+
+    fun paintClicked(view: View){
+        if(view !== mImageButtonCurrentPaint){
+            val imageButton = view as ImageButton
+            val colorTag = imageButton.tag.toString()
+
+            drawingView?.setColor(colorTag)
+
+            imageButton.setImageDrawable(
+                ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
+            )
+            mImageButtonCurrentPaint!!.setImageDrawable(
+                ContextCompat.getDrawable(this, R.drawable.pallet_normal)
+            )
+
+            mImageButtonCurrentPaint = view
+        }
+    }
+
 }

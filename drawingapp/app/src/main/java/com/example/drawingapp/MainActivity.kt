1ae03330f09823a4b9156ab24f8f5c92d0321ff9
BartoszRudnik <rudnik49@gmail.com>
2022-04-11 12:35:50 +0200

    drawingApp -> custom background image && undo button && redo button

--- drawingapp/app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ drawingapp/app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -1,19 +1,56 @@
 package com.example.drawingapp
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Intent
 import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.core.view.get
 import android.widget.LinearLayout
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+
 
 class MainActivity : AppCompatActivity() {
     private var drawingView: DrawingView? = null
-    private var mImageButtonCurrentPaint : ImageButton? = null
+    private var mImageButtonCurrentPaint: ImageButton? = null
+
+    private val openGalleryLauncher: ActivityResultLauncher<Intent> =registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        result ->
+        if(result.resultCode == RESULT_OK && result.data != null){
+            val imageBackGround : ImageView = findViewById(R.id.iv_background)
+
+            imageBackGround.setImageURI(result.data?.data)
+        }
+    }
+
+    private val requestPermission: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            permissions.entries.forEach {
+                val permissionName = it.key
+                val isGranted = it.value
+
+                if (!isGranted) {
+                    if (permissionName == Manifest.permission.READ_EXTERNAL_STORAGE) {
+
+                    }
+                } else {
+                    val pickIntent =
+                        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+
+                    openGalleryLauncher.launch(pickIntent)
+                }
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,24 +66,56 @@ class MainActivity : AppCompatActivity() {
 
         drawingView?.setSizeForBrush(20.toFloat())
 
-        val ibBrush : ImageButton = findViewById(R.id.ib_brush)
+        val ibBrush: ImageButton = findViewById(R.id.ib_brush)
 
         ibBrush.setOnClickListener {
             showBrushSizeChooserDialog()
         }
+
+        val ibGallery: ImageButton = findViewById(R.id.ib_gallery)
+
+        ibGallery.setOnClickListener {
+            requestStoragePermission()
+        }
+
+        val ibUndo: ImageButton = findViewById(R.id.ib_undo)
+
+        ibUndo.setOnClickListener {
+            drawingView!!.onClickUndo()
+        }
+
+        val ibRedo: ImageButton = findViewById(R.id.ib_redo)
+
+        ibRedo.setOnClickListener {
+            drawingView!!.onClickRedo()
+        }
     }
 
-    private fun showBrushSizeChooserDialog(){
+    private fun requestStoragePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this, Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+        ) {
+            showRationaleDialog(
+                "Kids Drawing App",
+                "Kids Drawing App " + "needs to Access Your external storage"
+            )
+        } else {
+            requestPermission.launch(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE))
+        }
+    }
+
+    private fun showBrushSizeChooserDialog() {
         val brushDialog = Dialog(this)
 
         brushDialog.setContentView(R.layout.dialog_brush_size)
         brushDialog.setTitle("Brush size: ")
 
-        val smallButton : ImageButton = findViewById(R.id.id_small_brush)
-        val mediumButton : ImageButton = findViewById(R.id.id_medium_brush)
-        val largeButton : ImageButton = findViewById(R.id.id_large_brush)
+        val smallButton: ImageButton = brushDialog.findViewById(R.id.id_small_brush)
+        val mediumButton: ImageButton = brushDialog.findViewById(R.id.id_medium_brush)
+        val largeButton: ImageButton = brushDialog.findViewById(R.id.id_large_brush)
 
-        smallButton.setOnClickListener{
+        smallButton.setOnClickListener {
             drawingView?.setSizeForBrush(10.toFloat())
             brushDialog.dismiss()
         }
@@ -64,8 +133,8 @@ class MainActivity : AppCompatActivity() {
         brushDialog.show()
     }
 
-    fun paintClicked(view: View){
-        if(view !== mImageButtonCurrentPaint){
+    fun paintClicked(view: View) {
+        if (view !== mImageButtonCurrentPaint) {
             val imageButton = view as ImageButton
             val colorTag = imageButton.tag.toString()
 
@@ -82,4 +151,14 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun showRationaleDialog(title: String, message: String) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+
+        builder.setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+        builder.create().show()
+    }
 }

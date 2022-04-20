6d7e343d71326aa6122789c783fcedc6a45fde65
BartoszRudnik <rudnik49@gmail.com>
2022-04-20 11:22:38 +0200

    happyPlacesApp -> loading image from device && taking picture from camera

--- happyPlaces/app/src/main/java/com/example/myapplication/AddHappyPlaceActivity.kt
+++ happyPlaces/app/src/main/java/com/example/myapplication/AddHappyPlaceActivity.kt
@@ -1,10 +1,24 @@
 package com.example.myapplication
 
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
 import android.app.DatePickerDialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.Settings
 import android.view.View
 import com.example.myapplication.databinding.ActivityAddHappyPlaceBinding
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -35,6 +49,7 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
             updateDateInView()
         }
         binding?.etDate?.setOnClickListener(this)
+        binding?.tvAddImage?.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -48,7 +63,113 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
                     cal.get(Calendar.DAY_OF_MONTH)
                 ).show()
             }
+            R.id.tv_add_image -> {
+                val pictureDialog = AlertDialog.Builder(this)
+                pictureDialog.setTitle("Select Action")
+
+                val pictureDialogItems =
+                    arrayOf("Select photo from Gallery", "Capture photo from camera")
+
+                pictureDialog.setItems(pictureDialogItems) { _, which ->
+                    when (which) {
+                        0 -> choosePhotoFromGallery()
+                        1 -> takePhotoFromCamera()
+                    }
+                }
+                pictureDialog.show()
+            }
         }
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == GALLERY) {
+                if (data != null) {
+                    val contentUri = data.data
+                    try {
+                        val selectedImageBitmap =
+                            MediaStore.Images.Media.getBitmap(this.contentResolver, contentUri)
+
+                        binding?.ivPlaceImage?.setImageBitmap(selectedImageBitmap)
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                }
+            } else if (requestCode == CAMERA) {
+                if (data != null) {
+                    val thumbnail: Bitmap = data.extras!!.get("data") as Bitmap
+
+                    binding?.ivPlaceImage?.setImageBitmap(thumbnail)
+                }
+            }
+        }
+    }
+
+    private fun takePhotoFromCamera() {
+        Dexter.withActivity(this).withPermissions(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA,
+        ).withListener(object : MultiplePermissionsListener {
+            override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                if (report!!.areAllPermissionsGranted()) {
+                    val cameraIntent =
+                        Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+                    startActivityForResult(cameraIntent, CAMERA)
+                }
+            }
+
+            override fun onPermissionRationaleShouldBeShown(
+                permissions: MutableList<com.karumi.dexter.listener.PermissionRequest>?,
+                token: PermissionToken?
+            ) {
+                showRationaleDialogForPermissions()
+            }
+        }).onSameThread().check()
+    }
+
+    private fun choosePhotoFromGallery() {
+        Dexter.withActivity(this).withPermissions(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ).withListener(object : MultiplePermissionsListener {
+            override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                if (report!!.areAllPermissionsGranted()) {
+                    val galleryIntent =
+                        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+
+                    startActivityForResult(galleryIntent, GALLERY)
+                }
+            }
+
+            override fun onPermissionRationaleShouldBeShown(
+                permissions: MutableList<com.karumi.dexter.listener.PermissionRequest>?,
+                token: PermissionToken?
+            ) {
+                showRationaleDialogForPermissions()
+            }
+        }).onSameThread().check()
+    }
+
+    private fun showRationaleDialogForPermissions() {
+        AlertDialog.Builder(this)
+            .setMessage("It looks like you have turned off permission required for this feature")
+            .setPositiveButton("GO TO SETTINGS") { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }.show()
     }
 
     private fun updateDateInView() {
@@ -56,5 +177,10 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
         val sdf = SimpleDateFormat(myFormat, Locale.getDefault())
 
         binding?.etDate?.setText(sdf.format(cal.time).toString())
+    }
+
+    companion object {
+        private const val GALLERY = 1
+        private const val CAMERA = 2
     }
 }

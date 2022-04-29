8196fd18b6851b9fd613af91baa065ec8af148ee
BartoszRudnik <rudnik49@gmail.com>
2022-04-29 11:16:36 +0200

    trelloCloneApp -> signIn user

--- trelloClone/app/src/main/java/com/example/trelloclone/activities/SignInActivity.kt
+++ trelloClone/app/src/main/java/com/example/trelloclone/activities/SignInActivity.kt
@@ -1,13 +1,19 @@
 package com.example.trelloclone.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowManager
+import android.widget.Toast
 import com.example.trelloclone.R
 import com.example.trelloclone.databinding.ActivitySignInBinding
+import com.google.firebase.auth.FirebaseAuth
 
-class SignInActivity : AppCompatActivity() {
+class SignInActivity : BaseActivity() {
     private var binding: ActivitySignInBinding? = null
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +27,47 @@ class SignInActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
 
+        auth = FirebaseAuth.getInstance()
+
         setupActionBar()
+
+        binding?.btnSignIn?.setOnClickListener { signInRegisteredUser() }
+    }
+
+    private fun signInRegisteredUser() {
+        val email = binding?.etEmail?.text.toString().trim { it <= ' ' }
+        val password = binding?.etPassword?.text.toString().trim { it <= ' ' }
+
+        if (validateForm(email, password)) {
+            showProgressDialog(resources.getString(R.string.please_wait))
+
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    startActivity(Intent(this, MainActivity::class.java))
+                } else {
+                    Toast.makeText(this, "Authentication failed", Toast.LENGTH_LONG).show()
+
+                }
+                hideProgressDialog()
+            }
+        }
+    }
+
+    private fun validateForm(email: String, password: String): Boolean {
+        return when {
+            TextUtils.isEmpty(email) -> {
+                showErrorSnackBar("Please enter a email")
+                false
+            }
+            TextUtils.isEmpty(password) -> {
+                showErrorSnackBar("Please enter a password")
+                false
+            }
+            else -> {
+                true
+            }
+        }
     }
 
     private fun setupActionBar() {

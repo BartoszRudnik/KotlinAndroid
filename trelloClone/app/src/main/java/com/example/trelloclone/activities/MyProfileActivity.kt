b37f7cea95dce2e078142e2b0435ee303cade283
BartoszRudnik <rudnik49@gmail.com>
2022-05-02 10:45:01 +0200

    trelloCloneApp -> uploading new user profile image

--- trelloClone/app/src/main/java/com/example/trelloclone/activities/MyProfileActivity.kt
+++ trelloClone/app/src/main/java/com/example/trelloclone/activities/MyProfileActivity.kt
@@ -1,14 +1,32 @@
 package com.example.trelloclone.activities
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.webkit.MimeTypeMap
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.example.trelloclone.R
 import com.example.trelloclone.databinding.ActivityMyProfileBinding
 import com.example.trelloclone.firebase.FireStoreClass
 import com.example.trelloclone.models.User
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
 class MyProfileActivity : BaseActivity() {
+    companion object {
+        private const val READ_STORAGE_PERMISSION_CODE = 1
+        private const val PICK_IMAGE_REQUEST_CODE = 2
+    }
+
+    private var mSelectedUserImage: Uri? = null
+    private var mProfileImageDownload: String = ""
     private var binding: ActivityMyProfileBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +38,61 @@ class MyProfileActivity : BaseActivity() {
         setupActionBar()
 
         FireStoreClass().signInUser(this)
+
+        binding?.ivUserImage?.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                showImageChooser()
+            } else {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    READ_STORAGE_PERMISSION_CODE
+                )
+            }
+        }
+
+        binding?.btnUpdate?.setOnClickListener {
+            if (mSelectedUserImage != null) {
+                uploadUserImage()
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == READ_STORAGE_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                showImageChooser()
+            } else {
+
+            }
+        }
+    }
+
+    private fun showImageChooser() {
+        var galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+
+        startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK && requestCode == PICK_IMAGE_REQUEST_CODE && data!!.data != null) {
+            mSelectedUserImage = data.data
+        }
+
+        Glide.with(this@MyProfileActivity).load(mSelectedUserImage).centerCrop()
+            .placeholder(R.drawable.ic_user_place_holder).into(binding?.ivUserImage!!)
     }
 
     private fun setupActionBar() {
@@ -47,5 +120,30 @@ class MyProfileActivity : BaseActivity() {
         binding?.etName?.setText(user.name)
         binding?.etEmail?.setText(user.email)
         binding?.etMobile?.setText(user.mobile.toString())
+    }
+
+    private fun uploadUserImage() {
+        showProgressDialog(resources.getString(R.string.please_wait))
+
+        if (mSelectedUserImage != null) {
+            val storageReference: StorageReference =
+                FirebaseStorage.getInstance().reference.child(
+                    "USER_IMAGE" + System.currentTimeMillis() + "." + getFileExtension(
+                        mSelectedUserImage
+                    )
+                )
+
+            storageReference.putFile(mSelectedUserImage!!).addOnSuccessListener { taskSnapshot ->
+                taskSnapshot.metadata!!.reference!!.downloadUrl.addOnSuccessListener { uri ->
+
+                    mProfileImageDownload = uri.toString()
+                    hideProgressDialog()
+                }
+            }
+        }
+    }
+
+    private fun getFileExtension(uri: Uri?): String? {
+        return MimeTypeMap.getSingleton().getExtensionFromMimeType(contentResolver.getType(uri!!))
     }
 }
